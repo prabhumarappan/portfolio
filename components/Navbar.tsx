e1269095d4062b2de14594eb9105b3b8075090ba
Prabhu <prabhum.794@gmail.com>
2023-03-11 23:06:29 -0800

    added navbar

---
+++ components/Navbar.tsx
@@ -0,0 +1,26 @@
+export default function Navbar() {
+    return (
+        <section className="min-h-screen xl:w-1/3 lg:w-2/4 md:w-4/5 sm:w-4/5 mx-auto">
+            {/* TODO: Hide this when sm or xs */}
+            <nav>
+                <ul className="mb-12 flex mx-auto justify-center bg-cyan-600 rounded-b-lg">
+                    <li className="p-5">
+                        <a href="/">Home</a>
+                    </li>
+                    <li className="p-5">
+                        <a href="/">About</a>
+                    </li>
+                    <li className="p-5">
+                        <a href="/">Work</a>
+                    </li>
+                    <li className="p-5">
+                        <a href="/">Projects</a>
+                    </li>
+                    <li className="p-5">
+                        <a href="/">Contact</a>
+                    </li>
+                </ul>
+            </nav>
+        </section>
+    );
+}
