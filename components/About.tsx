4b61dd398e35688b47e4c09cb5185211e05feecd
Prabhu <prabhum.794@gmail.com>
2023-03-12 23:47:05 -0700

    adding projects

--- components/About.tsx
+++ components/About.tsx
@@ -1,16 +1,16 @@
 export default function About() {
     return (
-        <section id="about" className="text-center p-10">
+        <section id="about" className="text-center p-10 h-screen">
             <h1 className="text-5xl">About Me</h1>
-            <div className="py-10 xl:w-2/4 mx-auto">
+            <div className="py-10 xl:w-2/4 mx-auto h-1/3">
                 <p className="text-xl ">I'm currently doing my Master of Science in Computer Science from Portland State University.</p>
                 <br/>
                 <p className="text-xl">Before this I worked as a Software Engineer for 5 years in Backend Development, Infrastructure, System Design and Architecture for distributed web applications.</p>
                 <br/>
                 <p className="text-xl">Engineer looking to solve interesting engineering and data problems.</p>
             </div>
-            <div>
-                <h1 className="text-5xl pb-10">Skills</h1>
+            <div className="pb-10 h-2/3">
+                <h1 className="text-5xl">Skills</h1>
                 <p className="text-xl"><b className="font-bold">Languages</b> - Python, Go, Javascript</p>
                 <p className="text-xl"><b className="font-bold">Databases</b> - MongoDB, Redis, InfluxDB, MySQL</p>
                 <p className="text-xl"><b className="font-bold">Message Queues</b> - ActiveMQ, RabbitMQ, SQS</p>
--- components/Projects.tsx
+++ components/Projects.tsx
@@ -1,8 +1,12 @@
 export default function Projects() {
     return (
         <section id="projects" className="text-center p-10">
-            <div>
-                <h1 className="text-5xl">Projects</h1>
+            <h1 className="text-5xl">Projects</h1>
+            <div className="flex justify-center">
+                <h2 className="text-2xl">CTran Data Pipeline and Visualization</h2>
+            </div>
+            <div className="flex justify-center">
+                <h2 className="text-2xl">CTran Data Pipeline and Visualization</h2>
             </div>
         </section>
     );
