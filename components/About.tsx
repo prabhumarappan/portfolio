9a1f3cfbc078161c3b6d088716cf380959216eea
Prabhu <prabhum.794@gmail.com>
2023-03-14 22:38:03 -0700

    fixed lint issue in about

--- components/About.tsx
+++ components/About.tsx
@@ -3,35 +3,57 @@ export default function About() {
         <section id="about" className="text-center">
             <div className="py-10 xl:w-2/4 mx-auto">
                 <h1 className="text-5xl">About Me</h1>
-                <p className="text-xl pt-10">I'm currently doing my Master of Science in Computer Science from Portland State University.</p>
-                <br/>
-                <p className="text-xl">Before this I worked as a Software Engineer for 5 years in Backend Development, Infrastructure, System Design and Architecture for distributed web applications.</p>
-                <br/>
-                <p className="text-xl">Engineer looking to solve interesting engineering and data problems.</p>
+                <p className="text-xl pt-10">
+                    I`&apos;`m currently doing my Master of Science in Computer
+                    Science from Portland State University.
+                </p>
+                <br />
+                <p className="text-xl">
+                    Before this I worked as a Software Engineer for 5 years in
+                    Backend Development, Infrastructure, System Design and
+                    Architecture for distributed web applications.
+                </p>
+                <br />
+                <p className="text-xl">
+                    Engineer looking to solve interesting engineering and data
+                    problems.
+                </p>
             </div>
             <div className="py-10 xl:w-2/3 mx-auto">
                 <h1 className="text-5xl">Skills</h1>
                 <ul className="py-10 pb-0 mx-auto flex flex-wrap">
                     <li className="p-4 m-2 bg-cyan-400 rounded-xl">Python</li>
                     <li className="p-4 m-2 bg-cyan-400 rounded-xl">Go</li>
-                    <li className="p-4 m-2 bg-cyan-400 rounded-xl">Javascript</li>
+                    <li className="p-4 m-2 bg-cyan-400 rounded-xl">
+                        Javascript
+                    </li>
                     <li className="p-4 m-2 bg-cyan-400 rounded-xl">Java</li>
                     <li className="p-4 m-2 bg-cyan-400 rounded-xl">MongoDB</li>
-                    <li className="p-4 m-2 bg-cyan-400 rounded-xl">PostgreSQL</li>
+                    <li className="p-4 m-2 bg-cyan-400 rounded-xl">
+                        PostgreSQL
+                    </li>
                     <li className="p-4 m-2 bg-cyan-400 rounded-xl">InfluxDB</li>
                     <li className="p-4 m-2 bg-cyan-400 rounded-xl">Redis</li>
                     <li className="p-4 m-2 bg-cyan-400 rounded-xl">MySQL</li>
                     <li className="p-4 m-2 bg-cyan-400 rounded-xl">ActiveMQ</li>
                     <li className="p-4 m-2 bg-cyan-400 rounded-xl">RabbitMQ</li>
                     <li className="p-4 m-2 bg-cyan-400 rounded-xl">SQS</li>
-                    <li className="p-4 m-2 bg-cyan-400 rounded-xl">Amazon Web Services</li>
-                    <li className="p-4 m-2 bg-cyan-400 rounded-xl">Google Cloud Platform</li>
+                    <li className="p-4 m-2 bg-cyan-400 rounded-xl">
+                        Amazon Web Services
+                    </li>
+                    <li className="p-4 m-2 bg-cyan-400 rounded-xl">
+                        Google Cloud Platform
+                    </li>
                     <li className="p-4 m-2 bg-cyan-400 rounded-xl">Docker</li>
                     <li className="p-4 m-2 bg-cyan-400 rounded-xl">Git</li>
                     <li className="p-4 m-2 bg-cyan-400 rounded-xl">FluentD</li>
                     <li className="p-4 m-2 bg-cyan-400 rounded-xl">Kibana</li>
-                    <li className="p-4 m-2 bg-cyan-400 rounded-xl">ElasticSearch</li>
-                    <li className="p-4 m-2 bg-cyan-400 rounded-xl">Microservices</li>
+                    <li className="p-4 m-2 bg-cyan-400 rounded-xl">
+                        ElasticSearch
+                    </li>
+                    <li className="p-4 m-2 bg-cyan-400 rounded-xl">
+                        Microservices
+                    </li>
                     <li className="p-4 m-2 bg-cyan-400 rounded-xl">Django</li>
                     <li className="p-4 m-2 bg-cyan-400 rounded-xl">React</li>
                     <li className="p-4 m-2 bg-cyan-400 rounded-xl">Next.js</li>
